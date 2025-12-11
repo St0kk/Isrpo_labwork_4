0800d47765e968c0581c0d12a6d2ee68570256f8
Georgiy <gosha.andreev77@gmail.com>
2025-12-11 12:38:52 +0300

    added CI/CD

--- geometric_lib_unit_test.py
+++ geometric_lib_unit_test.py
@@ -19,6 +19,14 @@ class CircleTestCase(unittest.TestCase):
         res = circle.area(2.4)
         self.assertEqual(res, 2.4 * 2.4 * math.pi)
     
+    def test_negative_radius_area(self):
+        res = circle.area(-2)
+        self.assertEqual(res, 4 * math.pi) 
+    
+    def test_negative_float_radius_area(self):
+        res = circle.area(-2.4)
+        self.assertEqual(res, 2.4 * 2.4 * math.pi) 
+
     def test_zero_radius_perimeter(self):
         res = circle.perimeter(0)
         self.assertEqual(res, 0)
@@ -30,6 +38,14 @@ class CircleTestCase(unittest.TestCase):
     def test_positive_float_radius_perimeter(self):
         res = circle.perimeter(2.4)
         self.assertEqual(res, 2 * 2.4 * math.pi)
+
+    def test_negative_int_radius_perimeter(self):
+        res = circle.perimeter(-2)
+        self.assertEqual(res, 2 * (-2) * math.pi)
+
+    def test_negative_float_radius_perimeter(self):
+        res = circle.perimeter(-2.4)
+        self.assertEqual(res, 2 * (-2.4) * math.pi)
 
 
 class RectangleTestCase(unittest.TestCase):
@@ -53,6 +69,18 @@ class RectangleTestCase(unittest.TestCase):
         res = rectangle.area(2, 4.2)
         self.assertEqual(res, 2 * 4.2)
     
+    def test_negative_sides_area(self):
+        res = rectangle.area(-2, 4)
+        self.assertEqual(res, -8) 
+    
+    def test_both_negative_sides_area(self):
+        res = rectangle.area(-2, -4)
+        self.assertEqual(res, 8)  
+    
+    def test_negative_float_sides_area(self):
+        res = rectangle.area(-2.4, 4.2)
+        self.assertEqual(res, -2.4 * 4.2)
+
     def test_zero_side_perimeter(self):
         res = rectangle.perimeter(0, 5)
         self.assertEqual(res, 10)
@@ -69,7 +97,17 @@ class RectangleTestCase(unittest.TestCase):
         res = rectangle.perimeter(2, 4.2)
         self.assertEqual(res, 2 * 2 + 2 * 4.2)
     
+    def test_negative_sides_perimeter(self):
+        res = rectangle.perimeter(-2, 4)
+        self.assertEqual(res, 2 * (-2) + 2 * 4)
     
+    def test_both_negative_sides_perimeter(self):
+        res = rectangle.perimeter(-2, -4)
+        self.assertEqual(res, 2 * (-2) + 2 * (-4))
+    
+    def test_negative_float_sides_perimeter(self):
+        res = rectangle.perimeter(-2.4, 4.2)
+        self.assertEqual(res, 2 * (-2.4) + 2 * 4.2)
 
 
 class SquareTestCase(unittest.TestCase):
@@ -85,6 +123,14 @@ class SquareTestCase(unittest.TestCase):
         res = square.area(3.2)
         self.assertEqual(res, 3.2 * 3.2)
     
+    def test_negative_side_area(self):
+        res = square.area(-3)
+        self.assertEqual(res, 9) 
+    
+    def test_negative_float_side_area(self):
+        res = square.area(-3.2)
+        self.assertEqual(res, 3.2 * 3.2) 
+
     def test_zero_side_perimeter(self):
         res = square.perimeter(0)
         self.assertEqual(res, 0)
@@ -96,6 +142,14 @@ class SquareTestCase(unittest.TestCase):
     def test_positive_side_float_perimeter(self):
         res = square.perimeter(3.2)
         self.assertEqual(res, 3.2 * 4)
+    
+    def test_negative_side_perimeter(self):
+        res = square.perimeter(-3)
+        self.assertEqual(res, -12)  
+    
+    def test_negative_float_side_perimeter(self):
+        res = square.perimeter(-3.2)
+        self.assertEqual(res, -3.2 * 4)
 
 
 class TriangleTestCase(unittest.TestCase):
@@ -121,6 +175,22 @@ class TriangleTestCase(unittest.TestCase):
         res = triangle.area(4, 6.4)
         self.assertEqual(res, (4 * 6.4) / 2)
     
+    def test_negative_base_area(self):
+        res = triangle.area(-4, 6)
+        self.assertEqual(res, -12) 
+    
+    def test_negative_height_area(self):
+        res = triangle.area(4, -6)
+        self.assertEqual(res, -12) 
+    
+    def test_both_negative_values_area(self):
+        res = triangle.area(-4, -6)
+        self.assertEqual(res, 12) 
+    
+    def test_negative_float_values_area(self):
+        res = triangle.area(-4.6, 6.4)
+        self.assertEqual(res, (-4.6 * 6.4) / 2)
+
     def test_zero_sides_perimeter(self):
         res = triangle.perimeter(0, 0, 0)
         self.assertEqual(res, 0)
@@ -141,3 +211,18 @@ class TriangleTestCase(unittest.TestCase):
         res = triangle.perimeter(3, 4.2, 5)
         self.assertEqual(res, 3 + 4.2 + 5)
     
+    def test_negative_sides_perimeter(self):
+        res = triangle.perimeter(-3, 4, 5)
+        self.assertEqual(res, -3 + 4 + 5)
+    
+    def test_multiple_negative_sides_perimeter(self):
+        res = triangle.perimeter(-3, -4, 5)
+        self.assertEqual(res, -3 + (-4) + 5)
+    
+    def test_all_negative_sides_perimeter(self):
+        res = triangle.perimeter(-3, -4, -5)
+        self.assertEqual(res, -3 + (-4) + (-5))
+    
+    def test_negative_float_sides_perimeter(self):
+        res = triangle.perimeter(-3.4, 4.5, 5.3)
+        self.assertEqual(res, -3.4 + 4.5 + 5.3)
